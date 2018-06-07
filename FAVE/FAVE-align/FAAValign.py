1d42f0194493a9e61d1c54aa90b6d45135ecdf99
Liberty Hamilton <libertyhamilton@gmail.com>
2018-06-07 12:45:10 -0500

    more tmp prints

--- FAVE/FAVE-align/FAAValign.py
+++ FAVE/FAVE-align/FAAValign.py
@@ -184,6 +184,7 @@ def align(wavfile, trs_input, outfile, FADIR='', SOXPATH='', HTKTOOLSPATH=''):
     print("Making tmp directory")
     os.mkdir("./tmp" + identifier)
     os.listdir(os.getcwd()+'/tmp'+identifier)
+    os.listdir('./tmp'+identifier)
     # prepare wavefile
     SR = prep_wav(wavfile, './tmp' + identifier + '/tmp' + identifier + '.wav', SOXPATH)
 
